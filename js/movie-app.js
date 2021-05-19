7aedc10da52aaa4c56868cd80c84fffe41755edf
Dustin Martinez <martinezdustin736@gmail.com>
2021-05-19 10:46:30 -0500

    Fixed prevent default -added rating

--- js/movie-app.js
+++ js/movie-app.js
@@ -5,7 +5,7 @@
 // Can use for the loading animation/message
 // let loader = `<div class="boxLoading"></div>`;
 // document.getElementById('movieResult').innerHTML = loader;
-
+$(document).ready(function (){
 const getMovies = () => {
     $('#container').html('loading')
     setTimeout(() => {
@@ -15,34 +15,53 @@ const getMovies = () => {
                 console.log(movies)
                 let htmlStr = '';
                 for(let movie of movies){
-                    htmlStr = `${htmlStr}<h1>${movie.title}</h1><p>by: ${movie.director}</p>`
+                    htmlStr = `${htmlStr}<h1>${movie.title}</h1><p>by: ${movie.director}</p><p>Rating: ${movie.rating}</p>`
                 }
                 $('#container').html(htmlStr)
             });
-    }, 3000)
+    }, 1500)
 
 }
-let newMovie = {
-    // "title": "Percy Jackson & The Lightning Thief",
-    // "author": {
-    //     "firstName": "Rick",
-    //     "lastName": "Riordan"
-    // }
-};
-let postOptions = {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(),
-}
+// let newMovie = {
+//     title: $('#movie-title').val(),
+//     director: $('#movie-director').val()
+// };
 
-$('#addMovie').click( (e) => {
-e.preventDefault()
+
+
+$('#addMovie').on("click", (e) => {
+    e.preventDefault();
+
+
+    let newMovie = {
+        title: $('#movie-title').val(),
+        director: $('#movie-director').val(),
+        rating:$("#movie-rate").val()
+    };
+    console.log("newMovie");
+    let postOptions = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newMovie),
+    }
+
     fetch("https://quiet-purring-yellowhorn.glitch.me/movies", postOptions)
-        // .then(resp => resp.json())
-        // .then(book => console.log(book));
-        .then(getMovies);
+        .then(resp => resp.json())
+        .then(movies => console.log(movies));
+
 })
 
 getMovies()
+    //
+    // let deleteOptions = {
+    //     method: 'DELETE',
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //     },
+    // }
+    //
+    // fetch("https://quiet-purring-yellowhorn.glitch.me/movies/10", deleteOptions).then(getMovies)
+
+});
