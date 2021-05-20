4a23e1538f09e74c414cf04db3e027d3d813a9ee
David Culver <culver.david.p@gmail.com>
2021-05-20 13:33:27 -0500

    completed edit movie functionality

--- js/movie-app.js
+++ js/movie-app.js
@@ -16,10 +16,55 @@ $(document).ready(function () {
                     console.log(movies)
                     for (let movie of movies) {
                         let htmlStr = `<h1>${movie.title}</h1><p>by: ${movie.director}</p><p>Rating: ${movie.rating}</p>`;
-                        htmlStr += `<button id="delete-${movie.id}" class="btn btn-primary deleteMovie">Delete</button>`
+                        htmlStr += `<button id="delete-${movie.id}" class="btn btn-primary deleteMovie">Delete</button>`;
+                        htmlStr += `<button id="edit-${movie.id}" class="btn btn-primary editMovie">Edit Movie</button>`;
+
+                        // create form to edit movie
+                        htmlStr += `<div id="editCurrentMovie-${movie.id}" style="display: none">`
+                        htmlStr += `<h3>Edit this movie</h3>`
+                        htmlStr += `<form id="editForm"><div class="form-group">`;
+                        htmlStr += `<label for="edit-title">Edit Movie Title</label>`;
+                        htmlStr += `<input value="${movie.title}" type="text" class="form-control" id="edit-title" aria-describedby="emailHelp" placeholder='${movie.title}'></div>`;
+                        htmlStr += `<div><label for="edit-director">Edit Movie Director</label>`;
+                        htmlStr += `<input value="${movie.director}" type="text" class="form-control" id="edit-director" aria-describedby="emailHelp" placeholder="${movie.director}">
+    </div>`;
+                        htmlStr += `<label for="edit-rate">Edit the Rating</label>`;
+                        htmlStr += `<select id="edit-rate" class="form-select" aria-label="Default select example">`;
+                        htmlStr += `<option>Rate your title</option>`;
+                        htmlStr += `<option value="1">One</option>`;
+                        htmlStr += `<option value="2">Two</option>`;
+                        htmlStr += `<option value="3" selected="selected">Three</option>`;
+                        htmlStr += `<option value="4">Four</option>`;
+                        htmlStr += `<option value="5">Five</option>`;
+                        htmlStr += `</select>`;
+                        htmlStr += `<button id="save-${movie.id}" class="btn btn-primary">Save</button></form></div>`
                         $('#container').append(htmlStr)
+                        //delete movie button
                         $(`#delete-${movie.id}`).click(function () {
                             fetch(`https://quiet-purring-yellowhorn.glitch.me/movies/${movie.id}`, deleteOptions).then(getMovies)
+                        })
+                        //Edit movie button
+                        $(`#edit-${movie.id}`).click(function () {
+                            $(`#editCurrentMovie-${movie.id}`).css('display', 'inline');
+                        })
+
+                        $(`#save-${movie.id}`).click(function (e) {
+                            e.preventDefault();
+                            let patchThis = {
+                                // "title": $('#edit-title').val(),
+                                "director": $('#edit-director').val(),
+                                "rating": $('#edit-rate').val()
+                            }
+
+                            let patchOptions = {
+                                method: 'PATCH',
+                                headers: {
+                                    'Content-Type': 'application/json',
+                                },
+                                //sending the info as json string
+                                body: JSON.stringify(patchThis),
+                            };
+                            fetch(`https://quiet-purring-yellowhorn.glitch.me/movies/${movie.id}`, patchOptions).then(getMovies);
                         })
                     }
 
@@ -29,6 +74,9 @@ $(document).ready(function () {
                             'Content-Type': 'application/json',
                         },
                     }
+
+
+
 
                 });
         }, 1500)
